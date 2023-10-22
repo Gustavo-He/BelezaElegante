e1a692c2f97ec7afeb0a0a2ae8f46ddb0115fdaf
Lactovacilo <jpe.bgi@gmail.com>
2023-10-22 17:43:44 -0300

    updateFinalEuAcho

--- main.py
+++ main.py
@@ -20,7 +20,7 @@
 
 
 import customtkinter as ctk
-import modulos
+import Modulos
 
 
 class App:
@@ -33,27 +33,25 @@ class App:
         self.screen.title("Beleza Elegante")
         self.screen.resizable(False, False)
 
-        self.tela1 = ctk.CTkFrame(self.screen, width=450, height=700)
-        self.tela2 = ctk.CTkFrame(self.screen, width=450, height=700)
-        self.tela3 = ctk.CTkFrame(self.screen, width=450, height=700)
-        self.telaConfirmada = ctk.CTkFrame(self.screen, width=450, height=700)
+        self.tela1 = Modulos.CriarFrame(self.screen, 0, 0, 450, 700)
+        self.tela2 = Modulos.CriarFrame(self.screen, 0, 1, 450, 700)
+        self.tela3 = Modulos.CriarFrame(self.screen, 0, 2, 450, 700)
+        self.telaConfirmada = Modulos.CriarFrame(self.screen, 0, 3, 450, 700)
+
+        # Tela 1
+        self.agendamentoData = Modulos.CriarCaixaDeTexto(self.tela1, 100, 20, 0, 1, Modo="Data")
+        self.agendamentoHora = Modulos.CriarCaixaDeTexto(self.tela1, 100, 20, 1, 1, Modo="Hora")
+        self.btnAgendar = Modulos.CriarBotão(self.tela1, "Agendar", self.agendar, 4, 0, 100, 50)
+
+
+        # Tela 2
 
     def start(self):
-        labelData = ctk.CTkLabel(self.tela1, text="Data")
-        labelData.grid(row=0, column=0, padx=10, sticky="e")
+        Modulos.CriarLabel(self.tela1, "Data", 0, 0)
 
-        self.agendamentoData = modulos.criar_entrada(self.tela1, 100, 20, 0, 1, modo="data")
+        Modulos.CriarLabel(self.tela1, "Hora", 1, 0)
 
-        labelHora = ctk.CTkLabel(self.tela1, text="Hora")
-        labelHora.grid(row=1, column=0, padx=10, sticky="e")
-
-        self.agendamentoHora = modulos.criar_entrada(self.tela1, 100, 20, 1, 1, modo="hora")
-
-        btnServicos = ctk.CTkButton(self.tela1, text="Serviços", command=self.entrar_tela2)
-        btnServicos.grid(row=2, pady=10, padx=10)
-
-        self.btnAgendar = ctk.CTkButton(self.tela1, text="Agendar", command=self.agendar)
-        self.btnAgendar.grid(row=4, pady=10, padx=10)
+        Modulos.CriarBotão(self.tela1, "Serviços", self.entrar_tela2, 2, 0, 100, 50)
 
         self.tela1.grid(row=0, column=0)
 
@@ -64,98 +62,72 @@ class App:
             self.entrar_tela3()
 
     def confirmar_agendamento(self):
-        labelData = ctk.CTkLabel(self.telaConfirmada, text="Data: "+self.agendamentoData.get())
-        labelData.grid(row=0)
+        Modulos.CriarLabel(self.telaConfirmada, "Data: "+self.agendamentoHora.get(), 0, 0)
+        Modulos.CriarLabel(self.telaConfirmada, "Hora: "+self.agendamentoHora.get(), 1, 0)
 
-        labelHora = ctk.CTkLabel(self.telaConfirmada, text="Hora: "+self.agendamentoHora.get())
-        labelHora.grid(row=1)
+        agendamentos_frame = ctk.CTkFrame(self.telaConfirmada)
+        agendamentos_frame.grid(row=2, column=0)
 
-        agendamentosFrame = ctk.CTkFrame(self.telaConfirmada)
-        servicoTitle = ctk.CTkLabel(agendamentosFrame, text="Serviços:")
-        servicoTitle.grid(row=0)
+        Modulos.CriarLabel(agendamentos_frame, "Serviços:", 0, 0)
 
         for index, nome in enumerate(self.agendamentos):
-            servicoLabel = ctk.CTkLabel(agendamentosFrame, text=nome)
-            servicoLabel.grid(row=(index + 1), padx=10)
+            Modulos.CriarLabel(agendamentos_frame, nome, (index+1), 0)
 
-        agendamentosFrame.grid(row=2)
-
-        nomeLabel = ctk.CTkLabel(self.telaConfirmada, text="Nome: "+self.nomeEntry.get())
-        nomeLabel.grid(row=3, column=0)
-
-        telLabel = ctk.CTkLabel(self.telaConfirmada, text="Telefone: "+self.telEntry.get())
-        telLabel.grid(row=4, column=0)
-
-        emailLabel = ctk.CTkLabel(self.telaConfirmada, text="E-mail: "+self.emailEntry.get())
-        emailLabel.grid(row=5, column=0)
+        Modulos.CriarLabel(self.telaConfirmada, "Nome: "+self.nomeEntry.get(), 3, 0)
+        Modulos.CriarLabel(self.telaConfirmada, "Telefone: "+self.telEntry.get(), 4, 0)
+        Modulos.CriarLabel(self.telaConfirmada, "E-mail: "+self.emailEntry.get(), 5, 0)
 
         self.tela3.grid_remove()
-        self.telaConfirmada.grid()
+        self.telaConfirmada.grid(row=0, column=0)
 
     def entrar_tela3(self):
-        labelData = ctk.CTkLabel(self.tela3, text="Data: "+self.agendamentoData.get())
-        labelData.grid(row=0)
-
-        labelHora = ctk.CTkLabel(self.tela3, text="Hora: "+self.agendamentoHora.get())
-        labelHora.grid(row=1)
+        Modulos.CriarLabel(self.tela3, "Data: "+self.agendamentoData.get(), 0, 0)
+        Modulos.CriarLabel(self.tela3, "Hora: "+self.agendamentoHora.get(), 1, 0)
 
         agendamentosFrame = ctk.CTkFrame(self.tela3)
-        servicoTitle = ctk.CTkLabel(agendamentosFrame, text="Serviços:")
-        servicoTitle.grid(row=0)
+        agendamentosFrame.grid(row=2, column=0)
+
+        Modulos.CriarLabel(agendamentosFrame, "Serviços:", 0, 0)
 
         for index, nome in enumerate(self.agendamentos):
-            servicoLabel = ctk.CTkLabel(agendamentosFrame, text=nome)
-            servicoLabel.grid(row=(index + 1), padx=10)
+            Modulos.CriarLabel(agendamentosFrame, nome, index+1, 0)
 
-        agendamentosFrame.grid(row=2)
+        Modulos.CriarLabel(self.tela3, "Nome:", 3, 0)
+        self.nomeEntry = Modulos.CriarCaixaDeTexto(self.tela3, 150, 20, 3, 1)
 
-        nomeLabel = ctk.CTkLabel(self.tela3, text="Nome:")
-        nomeLabel.grid(row=3, column=0)
+        Modulos.CriarLabel(self.tela3, "Telefone:", 4, 0)
+        self.telEntry = Modulos.CriarCaixaDeTexto(self.tela3, 150, 20, 4, 1, Modo="Telefone")
 
-        self.nomeEntry = modulos.criar_entrada(self.tela3, 150, 20, 3, 1)
+        Modulos.CriarLabel(self.tela3, "E-mail:", 5, 0)
+        self.emailEntry = Modulos.CriarCaixaDeTexto(self.tela3, 150, 20, 5, 1)
 
-        telLabel = ctk.CTkLabel(self.tela3, text="Telefone:")
-        telLabel.grid(row=4, column=0)
-
-        self.telEntry = modulos.criar_entrada(self.tela3, 150, 20, 4, 1, modo="telefone")
-
-        emailLabel = ctk.CTkLabel(self.tela3, text="E-mail:")
-        emailLabel.grid(row=5, column=0)
-
-        self.emailEntry = modulos.criar_entrada(self.tela3, 150, 20, 5, 1)
-
-        btnConfirmar = ctk.CTkButton(self.tela3, text="Confirmar agendamento", command=self.confirmar_agendamento)
-        btnConfirmar.grid(row=6, column=0)
+        Modulos.CriarBotão(self.tela3, "Confirmar agendamento", self.confirmar_agendamento, 6, 0, 80, 15)
 
         self.tela1.grid_remove()
-        self.tela3.grid()
+        self.tela3.grid(row=0, column=0)
 
     def entrar_tela2(self):
-        modulos.criar_servico(self.tela2, "Corte de cabelo", "Faz um corte daora no cabelo", "R$20", "16:00 - 19:00", 0, lambda: self.adicionar_a_reserva("Corte de cabelo"))
-        modulos.criar_servico(self.tela2, "Manicure", "Deixa a mão bonita", "R$70", "8:00 - 18:00", 1, lambda: self.adicionar_a_reserva("Manicure"))
-        modulos.criar_servico(self.tela2, "Pedicure", "Deixa o pé legal", "R$50", "5:00 - 14:00", 2, lambda: self.adicionar_a_reserva("Pedicure"))
-        modulos.criar_servico(self.tela2, "Maquiagem", "Faz maquiagem bonita", "R$200", "9:00 - 12:00", 3, lambda: self.adicionar_a_reserva("Maquiagem"))
+        Modulos.criar_servico(self.tela2, "Corte de cabelo", "Faz um corte daora no cabelo", "R$20", "16:00 - 19:00", 0, lambda: self.adicionar_a_reserva("Corte de cabelo"))
+        Modulos.criar_servico(self.tela2, "Manicure", "Deixa a mão bonita", "R$70", "8:00 - 18:00", 1, lambda: self.adicionar_a_reserva("Manicure"))
+        Modulos.criar_servico(self.tela2, "Pedicure", "Deixa o pé legal", "R$50", "5:00 - 14:00", 2, lambda: self.adicionar_a_reserva("Pedicure"))
+        Modulos.criar_servico(self.tela2, "Maquiagem", "Faz maquiagem bonita", "R$200", "9:00 - 12:00", 3, lambda: self.adicionar_a_reserva("Maquiagem"))
 
-        btnVoltarTela = ctk.CTkButton(self.tela2, text="Voltar", command=self.entrar_tela1)
-        btnVoltarTela.grid(row=4)
+        Modulos.CriarBotão(self.tela2, "Voltar", self.entrar_tela1, 4, 0, 80, 15)
 
-        self.tela2.grid(row=0, column=0)
         self.tela1.grid_remove()
+        self.tela2.grid(row=0, column=0)
 
     def entrar_tela1(self):
         agendamentosFrame = ctk.CTkFrame(self.tela1)
-
-        servicoTitle = ctk.CTkLabel(agendamentosFrame, text="Serviços:")
-        servicoTitle.grid(row=0)
-
-        for index, nome in enumerate(self.agendamentos):
-            servicoLabel = ctk.CTkLabel(agendamentosFrame, text=nome)
-            servicoLabel.grid(row=(index + 1), padx=10)
-
         agendamentosFrame.grid(row=3)
 
-        self.tela1.grid(row=0, column=0)
+        Modulos.CriarLabel(agendamentosFrame, "Serviços:", 0, 0)
+
+        for index, nome in enumerate(self.agendamentos):
+            Modulos.CriarLabel(agendamentosFrame, nome, index+1, 0)
+
         self.tela2.grid_remove()
+        self.tela1.grid(row=0, column=0)
 
     def adicionar_a_reserva(self, nomeReserva):
         for nome in self.agendamentos:
